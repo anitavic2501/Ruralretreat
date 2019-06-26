4c00d34b72c5ba96279f1e1b1d6398a48b7e0eeb
Anita <anitavictoria2501@gmail.com>
2019-06-26 16:20:53 +1200

    adminheader

--- partials/adminheader.php
+++ partials/adminheader.php
@@ -3,54 +3,80 @@
  /* Frame */
 
   position: absolute;
-  height: 55px;
-  left: 35px;
-  top: 33px;
+  height: 50px;
+  left: 5px;
 
   }
+    /*
+    ADDITIONAL DEMO STYLE, NOT IMPORTANT TO MAKE THINGS WORK BUT TO MAKE IT A BIT NICER :)
+*/
+@import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
 
+<style>
+body {
+  margin: 0;
+  font-family: "Lato", sans-serif;
+}
 
+.sidebar {
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+  position: fixed;
+  height: 100%;
+  overflow: auto;
+}
+
+.sidebar a {
+  display: block;
+  color: black;
+  padding: 16px;
+  text-decoration: none;
+}
+ 
+.sidebar a.active {
+  background-color: #4CAF50;
+  color: white;
+}
+
+.sidebar a:hover:not(.active) {
+  background-color: #555;
+  color: white;
+}
+
+div.content {
+  margin-left: 200px;
+  padding: 1px 16px;
+  height: 1000px;
+}
+
+@media screen and (max-width: 700px) {
+  .sidebar {
+    width: 100%;
+    height: auto;
+    position: relative;
+  }
+  .sidebar a {float: left;}
+  div.content {margin-left: 0;}
+}
+
+@media screen and (max-width: 400px) {
+  .sidebar a {
+    text-align: center;
+    float: none;
+  }
+}
   </style>
 
   
 
-<div id="headerlogo">
-            <table>
-                <tr>
-                    <td id="logo"><a  href="admindashboard.php">
-            <img class="logo_pic" src="images\logo.png"></td>
-         </table></tr>
-        </div>
-
-        <div class="wrapper">
-
-    <!-- Sidebar -->
-    <nav id="sidebar">
-        <div class="sidebar-header">
-            <h3>Bootstrap Sidebar</h3>
-        </div>
-
-        <ul class="list-unstyled components">
-            <p>Dummy Heading</p>
-            <li class="active">
-                <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Home</a>
-                
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-              
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-    </nav>
-
+<div class="sidebar">
+  <a  href="admindashboard.php">
+            <img class="logo_pic" src="images\logo.png"></a><hr>
+  <a href="adminusers.php">Manage Users</a>
+  <a href="adminbooking.php">Manage Bookings</a>
+  <a href="adminlistbooking.php">List Bookings</a>
+  <a href="logout.php">Logout</a>
 </div>
         
