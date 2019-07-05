9e1121f155f5dfe179dc214ed399e561c32157b2
fatinrmn <fatinroman@yandex.ru>
2019-07-05 14:09:19 +1200

    Update dbh.inc.php

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -1,8 +1,21 @@
 <?php
-$dBServername = "ruralretreat.cespegrr14df.ap-southeast-2.rds.amazonaws.com";
+$dBServername = "localhost";
 $dBUsername = "root";
-$dBPassword = "123anita";
+$dBPassword = "";
 $dBName = "ruralretreat";
+
+if (isset($_ENV['DATABASE_CONNECTION_URL'])) {
+    $dBServername = $_ENV['DATABASE_CONNECTION_URL'];
+}
+if (isset($_ENV['DATABASE_NAME'])) {
+    $dBName = $_ENV['DATABASE_NAME'];
+}
+if (isset($_ENV['DATABASE_USERNAME'])) {
+    $dBUsername = $_ENV['DATABASE_USERNAME'];
+}
+if (isset($_ENV['DATABASE_PASSWORD'])) {
+    $dBPassword = $_ENV['DATABASE_PASSWORD'];
+}
 
 // Create connection
  $conn = mysqli_connect($dBServername, $dBUsername, $dBPassword, $dBName);
