58dc910ef00b9c4fbeebafefeeaaa1523359dcf8
Anita <anitavictoria2501@gmail.com>
2019-07-12 11:22:08 +1200

    articles n moving style

--- create_post.php
+++ create_post.php
@@ -22,7 +22,7 @@ include 'partials/adminheader.php'; ?>
     <table class="table table-hover">
   <thead>
     <tr>
-	<br>
+	<br><br>
   <a href="manage_articles.php"> View Articles | </a>  
   <a href="create_post.php"> Create Posts | </a>
   <a href="posts.php"> Manage Articles | </a>
@@ -35,25 +35,25 @@ include 'partials/adminheader.php'; ?>
 	<title>Admin | Create Post</title>
 </head>
 <body>
-
+<div class="content">
 		<!-- Middle form - to create and edit  -->
 		<div class="action create-post-div">
-			<h2 class="page-title">Create/Edit Post</h2>
+			<h2 class="page-title" style="text-align: center;">Create/Edit Post</h2>
 			<form method="post" enctype="multipart/form-data" action="<?php echo 'create_post.php'; ?>" >
 				<!-- validation errors for the form -->
-				<?php include ('./includes/errors.php') ?>
+				<?php include './includes/errors.php' ?>
 
 				<!-- if editing post, the id is required to identify that post -->
 				<?php if ($isEditingPost === true): ?>
 					<input type="hidden" name="post_id" value="<?php echo $post_id; ?>">
 				<?php endif ?>
 
-				<input type="text" name="title" value="<?php echo $title; ?>" placeholder="Title"><br>
-				<label style="float: left; margin: 5px auto 5px;">Featured image</label><br><br>
+				<input type="text" name="title" value="<?php echo $title; ?>" placeholder="Title">
+				<label style="float: left; margin: 5px auto 5px;">Featured image</label>
 				<input type="file" name="featured_image" >
 				<textarea name="body" id="body" cols="30" rows="10"><?php echo $body; ?></textarea>
 				<select name="topic_id">
-					<option value="" selected disabled>Choose topic</option><br>
+					<option value="" selected disabled>Choose topic</option>
 					<?php foreach ($topics as $topic): ?>
 						<option value="<?php echo $topic['id']; ?>">
 							<?php echo $topic['name']; ?>
@@ -69,7 +69,7 @@ include 'partials/adminheader.php'; ?>
 							Publish
 							<input type="checkbox" value="1" name="publish" checked="checked">&nbsp;
 						</label>
-					<?php else: ?><br>
+					<?php else: ?>
 						<label for="publish">
 							Publish
 							<input type="checkbox" value="1" name="publish">&nbsp;
@@ -80,7 +80,7 @@ include 'partials/adminheader.php'; ?>
 				<!-- if editing post, display the update button instead of create button -->
 				<?php if ($isEditingPost === true): ?> 
 					<button type="submit" class="btn" name="update_post">UPDATE</button>
-				<?php else: ?><br>
+				<?php else: ?> <br>
 					<button type="submit" class="btn btn-primary" name="create_post">Save Post</button>
 				<?php endif ?>
 
